a8b2b8605f5ac67b372d8a5563ddb6eebc4afb20
RudyChin <bb1168kk@gmail.com>
2015-03-30 21:14:37 +0800

    two_sum increase performance
    let mergeSort sort a pair<value, index> array according to the
    value.

--- LeetCode/two_sum.c
+++ LeetCode/two_sum.c
@@ -16,37 +16,29 @@
  * Email: bb1168kk@gmail.com
  * 
  * Solution:
- *  First store the original array.
- *  Use a hashtable to store value to index hash.
- *    - If the value if negative use the second part of the hash
- *      table.
- *    - else use the first part of the hash table.
+ *  First store the original array into value index pair array.
  *  Use Merge Sort O(N log N) to sort the array.
  *  Go through the array, if target is lesser than current i and j
  *  then increase i and make j = i + 1.
  *  (There is no help to loop through the rest j)
- *  Use the answer value that just found to hash for indexes,
- *  search linked list if collision occurs.
+ *  if the sum matches target, find index value in that pair
  *
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-#define HASH_SIZE 2053
-#define MAGICNUM 1381
-
-typedef struct hash_node {
+typedef struct vipair {
+  int value;
   int index;
-  struct hash_node *next;
-} hash_node;
+} vipair;
 
 void 
-concat(int *arr, int l, int m, int r, int *sorted) {
+concat(vipair *arr, int l, int m, int r, vipair *sorted) {
   int idx = l;
   int x = l, y = m;
   while (x < m && y < r) {
-    if (arr[x] < arr[y])
+    if (arr[x].value < arr[y].value)
       sorted[idx++] = arr[x++];
     else
       sorted[idx++] = arr[y++];
@@ -58,13 +50,13 @@ concat(int *arr, int l, int m, int r, int *sorted) {
 }
 
 void
-copy(int *arr, int *sorted, int l, int r) {
+copy(vipair *arr, vipair *sorted, int l, int r) {
   for (l; l < r; l++)
     arr[l] = sorted[l];
 }
 
 void 
-mergeSplit(int *arr, int l, int r, int *sorted) {
+mergeSplit(vipair *arr, int l, int r, vipair *sorted) {
   if (r - l < 2) {
     return;
   }
@@ -76,8 +68,8 @@ mergeSplit(int *arr, int l, int r, int *sorted) {
 }
 
 void 
-mergeSort(int *arr, int n) {
-  int *sorted = (int *)malloc(sizeof(int)*n);
+mergeSort(vipair *arr, int n) {
+  vipair *sorted = (vipair*)malloc(sizeof(vipair)*n);
   for (int i = 0; i < n; i++)
     sorted[i] = arr[i];
   mergeSplit(arr, 0, n, sorted);
@@ -86,95 +78,27 @@ mergeSort(int *arr, int n) {
 
 int*
 twoSum(int numbers[], int n, int target) {
-  /** initial hashtable **/
-  hash_node *hashtable = (hash_node*)malloc(sizeof(hash_node)*HASH_SIZE*2);
-  for (int i = 0; i < 2*HASH_SIZE; i++) {
-    hashtable[i].index = 0;
-    hashtable[i].next = NULL;
-  }
-  /** store original array **/
-  int *original = (int*)malloc(sizeof(int)*n);
+  /** store it into vipair array **/
+  vipair *arr = (vipair*)malloc(sizeof(vipair)*n);
   for (int i = 0; i < n; i++) {
-    original[i] = numbers[i];
-  }
-
-  /** hash value to indexes before sort **/
-  for (int i = 0; i < n; i++) {
-    int temp = numbers[i] * MAGICNUM % HASH_SIZE;
-    if (temp < 0) {
-      temp = 2*HASH_SIZE + temp;
-    }
-    int index = temp;
-    //no collision
-    if (!hashtable[index].index) {
-      hashtable[index].index = i + 1;
-    }
-    // collision
-    else {
-      hash_node *iter = &hashtable[index];
-      while (iter->next) {
-        iter = iter->next;
-      }
-      iter->next = (hash_node*)malloc(sizeof(hash_node));
-      iter->next->index = i + 1;
-      iter->next->next = NULL;
-    }
+    arr[i].value = numbers[i];
+    arr[i].index = i+1;
   }
 
   /** sort first with nlogn algo **/
-  mergeSort(numbers, n);
+  mergeSort(arr, n);
 
   /** conditional compare **/
   int i = 0, j = 1;
   while (i < n) {
-    if (numbers[i] + numbers[j] > target) {
+    if (arr[i].value + arr[j].value > target) {
       i++;
       j = i + 1;
       continue;
-    } else if (numbers[i] + numbers[j] == target) {
+    } else if (arr[i].value + arr[j].value == target) {
       int *ans = (int *)malloc(sizeof(int)*2);
-      int hashidx1 = numbers[i] * MAGICNUM % HASH_SIZE; 
-      int idx1 = 0;
-      int idx2 = 0;
-      if (hashidx1 < 0) {
-        hashidx1 = 2*HASH_SIZE + hashidx1;
-      }
-      int hashidx2 = numbers[j] * MAGICNUM % HASH_SIZE;
-      if (hashidx2 < 0) {
-        hashidx2 = 2*HASH_SIZE + hashidx2;
-      }
-      //collision
-      if (hashidx1 == hashidx2) {
-        hash_node *iter = &hashtable[hashidx1];
-        while (iter) {
-          if (!idx1 && (original[(iter->index)-1] == numbers[i]))
-            idx1 = iter->index;
-          else if (!idx2 && (original[(iter->index)-1] == numbers[j]))
-            idx2 = iter->index;
-          else if (idx1 && idx2)
-            break;
-          iter = iter->next;
-        }
-      } else {
-        hash_node *iter1 = &hashtable[hashidx1];
-        hash_node *iter2 = &hashtable[hashidx2];
-        while (iter1) {
-          if (original[(iter1->index)-1] == numbers[i]) {
-            idx1 = iter1->index;
-            break;
-          } else {
-            iter1 = iter1->next;
-          }
-        }
-        while (iter2) {
-          if (original[(iter2->index)-1] == numbers[j]) {
-            idx2 = iter2->index;
-            break;
-          } else {
-            iter2 = iter2->next;
-          }
-        }
-      }
+      int idx1 = arr[i].index;
+      int idx2 = arr[j].index;
       if (idx1 > idx2) {
         ans[0] = idx2;
         ans[1] = idx1;
@@ -194,11 +118,14 @@ twoSum(int numbers[], int n, int target) {
 
 void
 testMerge() {
-  int arr[] = {-1,-2,-3,-4,-5};
+  vipair arr[5];
+  for (int i = 0; i < 5; i++) {
+    arr[i].value = 5-i;
+    arr[i].index = i+1;
+  }
   mergeSort(arr, 5);
   for (int i = 0; i < 5; i++)
-    printf("%d ", arr[i]);
-  printf("\n");
+    printf("value[%d] index[%d]\n", arr[i].value, arr[i].index);
 }
 
 int
