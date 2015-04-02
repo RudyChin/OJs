8cf8ed1c28cbdd7f07cfcc61553f8094d5251ad8
RudyChin <bb1168kk@gmail.com>
2015-04-02 18:25:05 +0800

    palindromic draft

---
+++ LeetCode/palindromic.c
@@ -0,0 +1,18 @@
+/*
+ * Given a string S, find the longest palindromic substring in S. 
+ * You may assume that the maximum length of S is 1000, and there exists one unique longest palindromic substring.
+ *
+ */
+
+/* 
+ * Authur: Rudy Chin
+ * Email: bb1168kk@gmail.com
+ * 
+ * Solution:
+ *
+ */
+
+char *longestPalindrome(char *s) {
+  int length = strlen(s);    
+  int mid = s/2;
+}
