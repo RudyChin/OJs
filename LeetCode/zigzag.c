e509b9907f89415f1304d6f8523e38178c193d86
Rudy Chin <bb1168kk@gmail.com>
2015-04-04 22:27:46 +0800

    zigzag init

---
+++ LeetCode/zigzag.c
@@ -0,0 +1,34 @@
+/*
+0   0   0
+0  00  00
+0 0 0 0 0
+00  00  0
+0   0   0
+*/
+
+/*
+ * The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: 
+ * (you may want to display this pattern in a fixed font for better legibility)
+ * P   A   H   N
+ * A P L S I I G
+ * Y   I   R
+ * And then read line by line: "PAHNAPLSIIGYIR"
+ * Write the code that will take a string and make this conversion given a number of rows:
+ *
+ * convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR".
+ *
+ */
+
+
+/* 
+ * Authur: Rudy Chin
+ * Email: bb1168kk@gmail.com
+ * 
+ * Solutions:
+ *
+ */
+
+char *convert(char *s, int nRows) {
+  int n = strlen(s);
+    
+}
