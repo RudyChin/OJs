22a2e630e786fe9a9c06ac00d314979501d151c7
RudyChin <bb1168kk@gmail.com>
2015-04-07 16:19:49 +0800

    init reverse_int.c

---
+++ LeetCode/reverse_int.c
@@ -0,0 +1,17 @@
+/*
+ * Reverse digits of an integer.
+ * Example1: x = 123, return 321
+ * Example2: x = -123, return -321
+ *
+ */
+
+/* 
+ * Authur: Rudy Chin
+ * Email: bb1168kk@gmail.com
+ * 
+ * Solutions:
+ *
+ */
+
+int reverse(int x) {
+}
