08278359f4333bf80f13d17e08b3f68d84146ba3
RudyChin <bb1168kk@gmail.com>
2015-04-08 04:31:54 +0800

    reverse integer, almost done but fail overflow

--- LeetCode/reverse_int.c
+++ LeetCode/reverse_int.c
@@ -13,5 +13,44 @@
  *
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+
+double mypow(int base, int exp) {
+  double sum = 1;
+  for (int i = 0; i < exp; i++)
+    sum *= base;
+  return sum;
+}
+
 int reverse(int x) {
+  int n = 0;
+  int negative = 0;
+  if (x < 0) {
+    negative = 1;
+    x = -x;
+  }
+  for (int temp = x / mypow(10, n); temp >= 10 || !temp; n+=2, temp = x / mypow(10, n)) {
+    if (!temp) {
+      n--;
+      break;
+    }
+  }
+  int sum = 0;
+  int i;
+  for (i = 0; i <= n; ++i) {
+    sum += (x / (int)mypow(10, n-i)) * (int)mypow(10, i);
+    x = x % (int)mypow(10, n-i);
+  }
+  //overflow
+  if (sum < 0)
+    return 0;
+  if (negative)
+    sum = -sum;
+  return sum;
+}
+
+int main() {
+  printf("%d\n", reverse(-2147483412));
+  return 0;
 }
