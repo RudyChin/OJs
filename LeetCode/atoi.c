b01ec14dae7dfabb7e647703f343b9141cc1b97e
RudyChin <bb1168kk@gmail.com>
2015-04-09 02:36:03 +0800

    atoi init

---
+++ LeetCode/atoi.c
@@ -0,0 +1,20 @@
+/* 
+ * Implement atoi to convert a string to an integer.
+ *
+ */
+
+/* 
+ * Authur: Rudy Chin
+ * Email: bb1168kk@gmail.com
+ * 
+ * Solutions:
+ *
+ */
+
+int myAtoi(char *str) {
+    
+}
+
+void atoiTest() {
+    
+}
