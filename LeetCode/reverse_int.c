6bfcdf70033360d0f7b13a37d0f82da3a823850e
RudyChin <bb1168kk@gmail.com>
2015-04-09 02:09:37 +0800

    finish reverse integer #7 in leetcode

--- LeetCode/reverse_int.c
+++ LeetCode/reverse_int.c
@@ -10,47 +10,55 @@
  * Email: bb1168kk@gmail.com
  * 
  * Solutions:
+ *   Make it positive and store the original sign
+ *   Find number of digits (multiply by 100 each round to speed up)
+ *   Loop through to extract digits from most significant one
+ *   Check if it is overflow
  *
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-double mypow(int base, int exp) {
-  double sum = 1;
+long mypow(int base, int exp) {
+  long sum = 1;
   for (int i = 0; i < exp; i++)
     sum *= base;
   return sum;
 }
 
 int reverse(int x) {
-  int n = 0;
-  int negative = 0;
+  int n = 0, negative = 0;
+
   if (x < 0) {
     negative = 1;
     x = -x;
+    //-2147483648
+    if (x < 0)
+      return 0;
   }
-  for (int temp = x / mypow(10, n); temp >= 10 || !temp; n+=2, temp = x / mypow(10, n)) {
+  //find number of digits
+  for (int temp = x / mypow(10, n); temp >= 10 || !temp; n+=2, temp = x / mypow(10, n))
     if (!temp) {
-      n--;
+      --n;
       break;
     }
-  }
-  int sum = 0;
+
+  long sum = 0;
   int i;
   for (i = 0; i <= n; ++i) {
-    sum += (x / (int)mypow(10, n-i)) * (int)mypow(10, i);
-    x = x % (int)mypow(10, n-i);
+    sum += (x / mypow(10, n-i)) * mypow(10, i);
+    x = x % mypow(10, n-i);
   }
   //overflow
-  if (sum < 0)
+  if (sum > (long)((1<<31)-1))
     return 0;
   if (negative)
     sum = -sum;
-  return sum;
+  return (int)sum;
 }
 
 int main() {
-  printf("%d\n", reverse(-2147483412));
+  printf("%d\n", reverse(-2147483648));
   return 0;
 }
