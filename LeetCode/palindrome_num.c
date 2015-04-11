ec8f6b2d9239c28fc43b02e776791f0d0107d3a1
RudyChin <bb1168kk@gmail.com>
2015-04-11 14:41:45 +0800

    Add negative test cases, pass the OJ

--- LeetCode/palindrome_num.c
+++ LeetCode/palindrome_num.c
@@ -8,9 +8,20 @@
  * Email: bb1168kk@gmail.com
  * 
  * Solutions:
+ *   Negative numbers can't be palindromic
+ *   First calculate how many digits are there
+ *   Then loop through to compare First and Last digit
  *
  */
 #include <stdio.h>
+
+#define ASSERT_EQ(X, Y) \
+  do { \
+    if ((X) == (Y)) \
+      printf("%s\n", "Pass"); \
+    else \
+      printf("%s\n", "Fail"); \
+  } while (0)
 
 int pow(int base, int exp)
 {
@@ -32,38 +43,44 @@ int calDigits(int x)
 }
 
 bool isPalindrome(int x) {
+  if (x < 0) return false;
   int digits = calDigits(x);
   int i;
-  for (i = 0; i < digits; ++i)
+  for (i = digits-1; i > 0; i-=2)
   {
-    if ((x % pow(10, digits-i-1)) != (x % pow(10, i)))
+    if ((x / pow(10, i)) != (x % 10))
       return false;
+    x %= pow(10, i);
+    x /= 10;
   }
   return true;
 }
 
-void assert(int num, bool expected)
-{
-  bool pass = (isPalindrome(num)==expected);
-  printf("Test:[%d] Result:[%s]\n", num, pass?"Pass":"Fail"); 
-  if (!pass)
-  {
-    printf("Output:[%s] Expected:[%s]\n\n", isPalindrome(num)?"True":"False", expected?"True":"False"); 
-  }
-}
-
 void testIsPalindrome()
 {
-  assert(12321, true);
-  assert(1, true);
-  assert(0, true);
-  assert(111111, true);
-  assert(123456, false);
-  assert(2147483647, false);
+  ASSERT_EQ(isPalindrome(12321), true);
+  ASSERT_EQ(isPalindrome(1), true);
+  ASSERT_EQ(isPalindrome(0), true);
+  ASSERT_EQ(isPalindrome(111111), true);
+  ASSERT_EQ(isPalindrome(123456), false);
+  ASSERT_EQ(isPalindrome(2147483647), false);
+  ASSERT_EQ(isPalindrome(-23432), false);
+  ASSERT_EQ(isPalindrome(-238432), false);
+  ASSERT_EQ(isPalindrome(-2147483648), false);
+  ASSERT_EQ(isPalindrome(-2147447412), false);
+}
+
+void testCalDigits()
+{
+  ASSERT_EQ(calDigits(12345678), 8); 
+  ASSERT_EQ(calDigits(12321), 5); 
 }
 
 int main(int argc, char *argv[])
 {
+  printf("calDigits()\n");
+  testCalDigits();
+  printf("isPalindrome()\n");
   testIsPalindrome();
   return 0;
 }
